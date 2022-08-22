a3acc3fdff9b9970d88b7f97fedd1203515cae82
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-22 16:55:38 -0400

    Get dog API

--- js/main.js
+++ js/main.js
@@ -14,3 +14,19 @@ function getQuote() {
 			console.log(`error ${err}`);
 		});
 }
+//Get dog API - The Dog API
+document.querySelector(".btn-dog").addEventListener("click", getDog);
+
+function getDog() {
+	const url = `https://api.thedogapi.com/v1/images/search?x-api-key=live_yGGMgLaViMEb5L8FpzQWxu1yZDqLHCZblYXpBsMUbzb8dX5o85nFoGxErjxcRVwL`;
+
+	fetch(url)
+		.then((res) => res.json()) // parse response as JSON
+		.then((response) => {
+			console.log(response);
+			document.querySelector(".dog").src = response[0].url;
+		})
+		.catch((err) => {
+			console.log(`error ${err}`);
+		});
+}
